a5bbdbc52dca01c332ea6a61ab8c76b88a10edeb
Lucas Alava Peña <lucas.alavapena@gmail.com>
2023-04-20 01:36:03 +0200

    Full WIP

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,100 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// state 0 for off
+// state -1 for on
+
+// TODO WHY AM I NOT USING A lol
+
+@state
+M=0
+@prevstate
+M=0
+
+@16383
+D=A
+
+@startoffset
+M=D
+
+@24576
+D=A
+
+@endoffset
+M=D
+
+
+(LOOP)
+    // update previous state
+    @state
+    D=M
+    @prevstate
+    M=D
+
+    // get keyboard input
+    @KBD 
+    D=M
+
+    @UPDATESTATE
+    0;JMP
+
+    (TEST)
+    @STATEDIFFIND
+    0;JMP
+
+    @UPDATE
+    D;JNE
+
+(UPDATESTATE)
+    @state
+    M=0
+
+    @TEST
+    D;JEQ
+
+    @state
+    M=-1
+(STATEDIFFIND)
+    @state
+    D=M
+
+    @prev
+    D=D-M
+
+    @CHANGE
+    D;JNE
+
+(UPDATE)
+    // Change needed
+    @startoffset
+    D=M
+    @i
+    M=D
+
+    @DRAW
+    0;JMP
+
+//DRAW
+(DRAW)
+    // increment counter
+    @i
+    D=M+1
+    M=D
+
+
+    @endoffset
+    D=M-D
+    // check if we finished the screen
+    @LOOP
+    D;JEQ
+
+    @state
+    D=M
+
+    @i
+    A=M
+    M=D
+
+    @DRAW
+    0;JMP
